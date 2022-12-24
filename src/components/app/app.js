bc27f40cf4e72e6e0d8757bd7b54bdfa1e301f07
airatmm <airatmm@gmail.com>
2022-12-24 21:59:09 +0300

    made Loader

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,11 +2,13 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import Constructor from '../constructor/constructor';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAllIngredientsData } from "../../services/slices/ingredients-slice";
+import Loader from "../loader/loader";
 
 const App = () => {
+    const isIngredients = useSelector(state => state.ingredients.itemsSuccess)
     const path = useLocation().pathname;
     const dispatch = useDispatch();
 
@@ -18,7 +20,7 @@ const App = () => {
         <div className={ styles.app }>
             <Routes>
                 <Route path="/" element={ <AppHeader path={ path } /> }>
-                    <Route path="/" element={ <Constructor /> } />
+                    <Route path="/" element={ !isIngredients ? <Loader/>  : <Constructor />} />
                 </Route>
             </Routes>
         </div>
